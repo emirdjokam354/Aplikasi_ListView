8331d9b90501f2778f1abf4169125fccc5324627
emirothman22 <emirothman22@gmail.com>
2022-07-29 13:26:16 +0700

    creating application calculator with android

---
+++ app/src/main/java/com/arne/aplikasilistview/MainActivity.java
@@ -0,0 +1,42 @@
+ package com.arne.aplikasilistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+ public class MainActivity extends AppCompatActivity {
+    // mendeklarasikan listview var dan menginisialisasi array tipe data string
+    private ListView lvItem;
+    private String[] namanegara = new String[] {
+        "Indonesia", "Malaysia", "Singapora", "Italia",
+        "Inggris", "Belanda", "Argentina", "Chile", "Mesir", "Uganda"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("ListView Sederhana"); // tampil judul
+
+        // membinding atau memformat data
+        lvItem = (ListView) findViewById(R.id.listview);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, namanegara);
+
+        // menset data didalam listview
+        lvItem.setAdapter(adapter);
+
+        lvItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Memilih : "+namanegara[position], Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+}
